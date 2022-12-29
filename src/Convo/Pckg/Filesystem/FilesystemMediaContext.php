bfd9bd8bd8b1ec247488000f537098987f5000b5
Tihomir Dmitrovic <tole.car@gmail.com>
2022-12-29 15:01:26 +0100

    Cleanup unused

--- src/Convo/Pckg/Filesystem/FilesystemMediaContext.php
+++ src/Convo/Pckg/Filesystem/FilesystemMediaContext.php
@@ -2,7 +2,6 @@
 
 namespace Convo\Pckg\Filesystem;
 
-use Convo\Core\Media\Mp3Id3File;
 use Convo\Core\Workflow\AbstractMediaSourceContext;
 
 class FilesystemMediaContext extends AbstractMediaSourceContext
@@ -79,9 +78,7 @@ class FilesystemMediaContext extends AbstractMediaSourceContext
         }
 
 
-
         $model          =   $this->_getQueryModel();
-
         $args           =   [
             'search' => $search,
             'search_folders' => $search_folders,
@@ -94,41 +91,19 @@ class FilesystemMediaContext extends AbstractMediaSourceContext
             return new \ArrayIterator( $this->_loadedSongs);
         }
         
-        
-        
         if ( $args_changed) {
             $this->_logger->info( 'Arguments changed. Storing them and rewinding results ...');
             $model['arguments']     =   $args;
             $model['post_index']    =   0;
         }
         
-        
-        $this->_loadedSongs     =   [];
-
         $this->_logger->info( 'Scanning dir ['.$base_path.'] against ['.$search.']['.$search_folders.'] with min match percentage ['.$min_match_percentage.']');
 
-        
         $provider   =   new Mp3InfoProvider( $this->_logger, $base_url, $artwork, $background);
         $filter     =   new Mp3Filter( $this->_logger, $provider, $min_match_percentage, $search, $search_folders);
         $dir_reader =   new Mp3DirectoryReader( $this->_logger, $provider, $filter);
         
         $this->_loadedSongs = $dir_reader->readFolder( $base_path);
-        
-//         $this->_mp3FileDirectory = new Mp3FileDirectory( $this->_logger);
-//         $this->_mp3FileDirectory->setBasePath($base_path);
-//         $this->_mp3FileDirectory->setBaseUrl($base_url);
-//         $this->_mp3FileDirectory->setMinMatchPercentage($min_match_percentage);
-        
-//         if (!empty($args['search']) || !empty($args['search_folders']) ) {
-//             $this->_mp3FileDirectory->filter($args);
-//         } else {
-//             $this->_mp3FileDirectory->filter();
-//         }
-//         $this->_mp3FileDirectory->sort($args);
-
-//         foreach ($this->_mp3FileDirectory->getItems() as $item) {
-//             $this->_loadedSongs[] = new Mp3Id3File( $item['real_path'], $item['file_url'], $artwork, $background);
-//         }
         
         $count              =   count( $this->_loadedSongs);
         $count_changed      =   count( $model['playlist']) !== $count;
@@ -153,7 +128,7 @@ class FilesystemMediaContext extends AbstractMediaSourceContext
             }
         }
 
-        $this->_logger->debug('Showing the contents of hte query model ['.print_r($model, true).']');
+        $this->_logger->debug( 'Showing the contents of hte query model ['.print_r( $model, true).']');
         $this->_saveQueryModel( $model);
         
         return new \ArrayIterator( $this->_loadedSongs);
